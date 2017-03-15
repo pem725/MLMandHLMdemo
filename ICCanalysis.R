130c8cb00f7d6c050e1c0f46cbad6b1b705bbb28
pem725 <pem725@gmail.com>
2017-03-14 23:08:42 -0400

    added a little more detail and pushed the limits to the ICC

--- ICCanalysis.R
+++ ICCanalysis.R
@@ -11,7 +11,15 @@ sang <- read.spss("./Sex Anger dataset for daily reliability 022517.sav",F,T)
 lme.NA <- lmer(dailyna~1 + (1|subjno) + (1|day),data=sang)
 lme.PA <- lmer(dailypa~1 + (1|subjno) + (1|day),data=sang)
 
+lme.NA2 <- lmer(dailyna~day+(day|subjno),data=sang)
+lme.PA2 <- lmer(dailypa~day+(day|subjno),data=sang)
+
 summary(lme.NA)
+summary(lme.PA)
 
 lmerICCest(lme.NA)
+lmerICCest(lme.NA2)
 lmerICCest(lme.PA)
+lmerICCest(lme.PA2)
+
+table(sang$Item)
